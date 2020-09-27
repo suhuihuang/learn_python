be62520b8f473778c5fb3fb22b8c704adcb8cb8c
苏辉煌 <suhuihuang@mobboy.cn>
2020-09-27 10:19:10 +0800

    add number

--- python_base/number.py
+++ python_base/number.py
@@ -1,3 +1,18 @@
 # 浮点型
 a = 8/5
-print(a)
+print("a = ",a)
+
+# 得到整数部分
+b = 8//5
+print("b =",b)
+
+b == 8//5
+print(type(b))
+
+# % 得到余数
+c = 8%5
+print("c = ",c)
+
+# 计算乘方
+d = 2**3
+print("d = ",d)
