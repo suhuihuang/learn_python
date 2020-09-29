cc176124855e43610cefa51123e1815b93f87118
苏辉煌 <suhuihuang@mobboy.cn>
2020-09-29 09:42:21 +0800

    add maping

--- learn_code/mapping.py
+++ learn_code/mapping.py
@@ -2,11 +2,11 @@ import turtle
 import turtle as p
 
 def drawCircle(x,y,c='red'):
-    p.pu()      # 抬起画笔
-    p.goto(x,y) # 绘制圆的起始位置
-    p.pd()      # 放下画笔
+    p.pu()        # 抬起画笔
+    p.goto(x, y)  # 绘制圆的起始位置
+    p.pd()        # 放下画笔
     p.color(c)
-    p.circle(30,360)
+    p.circle(30, 360)
 
 
 
